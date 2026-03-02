deec704b4fbf53e17208ddb99ed71bc7372ecb76
Kheldin <kheldin.acherchour@gmail.com>
2026-03-02 10:47:16 +0100

    1 done

--- ex1/higher_magic.py
+++ ex1/higher_magic.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Any
 
 def fireball(target: str) -> str:
     return f"Fireball hits {target}"
@@ -26,11 +26,34 @@ def power_amplifier(base_spell: Callable[..., int],
 def base_spell() -> int:
     return 4
 
+def conditional_caster(condition: Callable[..., bool],
+                       spell: Callable[..., Any]
+                       ) -> Callable[..., Any]:
+    return lambda *args: spell(*args) if (condition(*args)) else 'Spell fizzled'
+
+def test_bool_func(target: str) -> bool:
+    return True
+
+def spell_sequence(spells: list[Callable[..., Any]]) -> Callable[..., Any]:
+    def casting_spells(*args: str) -> list[Any]:
+        return [spell(*args) for spell in spells]
+    return casting_spells
+
 if __name__ == "__main__":
     print("=============Testing spell combiner=============")
     combined: Callable[..., tuple[str, str]] = spell_combiner(fireball, heal)
     print(f"Combined spell result: {combined('Dragon')}\n")
 
-    print("=============Power amplifier=============")
+    print("=============Testing Power amplifier=============")
     power_multiplier = power_amplifier(base_spell, 5)
     print(power_multiplier())
+
+    print()
+    print("=============Testing Conditional caster=============")
+    test_cond = conditional_caster(test_bool_func, fireball)
+    print(test_cond("Dragon"))
+
+    print()
+    print("=============Testing Spell sequence=============")
+    cast_multiple_spells = spell_sequence([fireball, heal])
+    print(cast_multiple_spells("Dragon"))

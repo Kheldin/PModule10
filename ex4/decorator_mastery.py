b45c7a349e8aa02cf2d6c4621ad1bc25f5301243
Kheldin <kheldin.acherchour@gmail.com>
2026-03-02 17:38:27 +0100

    save

--- ex4/decorator_mastery.py
+++ ex4/decorator_mastery.py
@@ -15,7 +15,6 @@ def spell_timer(func: Callable[..., Any]) -> Callable[..., Any]:
         return func()
     return wrapper
 
-
 @spell_timer
 def fireball() -> str:
     return ("Fireball cast !")
@@ -25,6 +24,27 @@ def power_validator(min_power: int) -> Callable[..., Any]:
         def wrapper_bis(*args: Any, **kwargs: Any) -> str | None:
             return (func(*args,**kwargs) if args[2] > min_power else 
                     "Insufficient power for this spell")
+        return wrapper_bis
+    return wrapper
+
+def retry_spell(max_attempts: int) -> Callable[..., Any]:
+    # @wraps()
+    nb_tries = 1
+    def wrapper(func: Callable[..., Any]):
+        def wrapper_bis(*args: Any, **kwargs: Any) -> None:
+            nonlocal nb_tries
+            save_tries = nb_tries
+            while nb_tries <= max_attempts:
+                try:
+                    func(*args, **kwargs)
+                except Exception:
+                    print(f"Spell failed, retrying... "
+                        f"(attempt {nb_tries}/{max_attempts})")
+                    nb_tries += 1
+                if nb_tries > max_attempts:
+                    print("Spell casting failed after max_attempts attempts")
+                if nb_tries == save_tries:
+                    break
         return wrapper_bis
     return wrapper
 
@@ -46,3 +66,18 @@ if __name__ == "__main__":
     mage = MageGuild()
     fireball()
     print(mage.cast_spell("fireball", 12))
+
+    print("\n=============Testing retry spell=============")
+
+    print("\nTesting with error")
+    @retry_spell(max_attempts=5)
+    def raise_error() -> None:
+        raise ValueError
+    raise_error()
+
+    print("\nTesting with no error")
+    @retry_spell(max_attempts=3)
+    def no_error() -> None:
+        print("No problemo")
+
+    no_error()
